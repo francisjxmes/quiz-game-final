afd39956f0c700922b4693d3523c3a67b87e57b6
francisjxmes <francissalinger25@gmail.com>
2025-09-24 20:16:06 +0100

    Add quiz questions data structure

--- run.py
+++ run.py
@@ -1,3 +1,41 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+quiz_questions = [
+    {
+        "question": "What is the capital of Canada?",
+        "answer": "ottawa"
+    },
+    {
+        "question": "Which river flows through Cairo?",
+        "answer": "nile"
+    },
+    {
+        "question": "What country has the most natural lakes?",
+        "answer": "canada"
+    },
+    {
+        "question": "Who played Iron Man in the Marvel Cinematic Universe?",
+        "answer": "robert downey jr"
+    },
+    {
+        "question": (
+            "What is the name of the Netflix series featuring a chess "
+            "prodigy named Beth Harmon?"
+        ),
+        "answer": "the queen's gambit"
+    },
+    {
+        "question": "Which artist released the hit song 'Bad Guy' in 2019?",
+        "answer": "billie eilish"
+    },
+    {
+        "question": "Who is the current president of the United States?",
+        "answer": "donald trump"
+    },
+    {
+        "question": "Which country exited the European Union in 2020?",
+        "answer": "united kingdom"
+    },
+    {
+        "question": "What international organization is abbreviated as 'UN'?",
+        "answer": "united nations"
+    }
+]
